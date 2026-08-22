23a817b1630d75fa7830b93a5c16dfa52ba62df5
Sangjae Lee <robin040416@gmail.com>
2026-08-22 18:59:28 -0400

    initial commit with threesum.cs code for leetcode 15. Will be commiting the leetcode progresses here

---
+++ LeetCodeAnswers/threesum.cs
@@ -0,0 +1,58 @@
+﻿namespace LeetCodeAnswers;
+
+public class threesum
+{
+    public IList<IList<int>> ThreeSum(int[] nums)
+    {
+        nums.Sort();
+        IList<IList<int>> result = new List<IList<int>>();
+
+        for (int i = 0; i < nums.Length-2; i++)
+        {
+            if (i > 0 && nums[i] == nums[i - 1])
+            {
+                continue;
+            }
+
+            if (nums[i] > 0)
+            {
+                break;
+            }
+            
+            int left = i + 1;
+            int right = nums.Length - 1;
+
+            while (left < right)
+            {
+                if (nums[i] + nums[left] + nums[right] == 0)
+                {
+                    result.Add(new List<int> { nums[i], nums[left], nums[right] });                    
+                    left++;
+                    right--;
+                    
+                    while (left < right && nums[left] == nums[left-1])
+                    {
+                        left++;
+                    }
+
+                    while (left < right && nums[right] == nums[right+1])
+                    {
+                        right--;
+                    }
+                }
+                else if (nums[i] + nums[left] + nums[right] < 0)
+                {
+                    left++;
+                }
+                else
+                {
+                    right--;
+                }
+
+                
+            }
+        }
+
+        return result;
+    }
+}
