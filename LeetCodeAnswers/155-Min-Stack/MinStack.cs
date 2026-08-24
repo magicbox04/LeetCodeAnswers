492394eb781d1827a7822dc7f74b42a76d220c00
Sangjae Lee <robin040416@gmail.com>
2026-08-24 13:31:28 -0400

    Finish Minstack

---
+++ LeetCodeAnswers/155-Min-Stack/MinStack.cs
@@ -0,0 +1,36 @@
+﻿namespace LeetCodeAnswers._155_Min_Stack;
+
+public class MinStack {
+    private Stack<int> stack;
+    private Stack<int> minStack;
+    
+    public MinStack() {
+        stack = new Stack<int>();
+        minStack = new Stack<int>();
+    }
+    
+    public void Push(int value) {
+        stack.Push(value);
+        if (minStack.Count == 0 || minStack.Peek() > value)
+            minStack.Push(value);
+        else
+        {
+            minStack.Push(minStack.Peek());
+        }
+    }
+    
+    public void Pop() {
+        stack.Pop();
+        minStack.Pop();
+    }
+    
+    public int Top()
+    {
+        return stack.Peek();
+    }
+    
+    public int GetMin()
+    {
+        return minStack.Peek();
+    }
+}
