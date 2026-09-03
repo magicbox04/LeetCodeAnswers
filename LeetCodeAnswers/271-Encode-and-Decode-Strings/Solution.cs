8018e1b60ae241398ca41fe9572b676c45e19384
Sangjae Lee <robin040416@gmail.com>
2026-09-02 22:08:41 -0400

    try encode and decode string

---
+++ LeetCodeAnswers/271-Encode-and-Decode-Strings/Solution.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCodeAnswers._271_Encode_and_Decode_Strings;
+
+public class Solution
+{
+    public string Encode(IList<string> strs)
+    {
+        string result = "";
+        foreach (var str in strs)
+        {
+            result += str.Length + "#" + str;
+        }
+        return result;
+    }
+
+
+    public List<string> Decode(string s)
+    {
+        List<string> result = new List<string>();
+        int i = 0;
+
+        while (i < s.Length)
+        {
+            int hashIndex = s.IndexOf('#', i);
+            int len = int.Parse(s.Substring(i, hashIndex - i));
+            result.Add(s.Substring(hashIndex+1, len));
+            i = hashIndex + len + 1;
+        }
+        return result;
+    }
+
+}
