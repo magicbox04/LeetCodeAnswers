56a1df69f9bd30595f06aaacf3927a7705188eb3
Sangjae Lee <robin040416@gmail.com>
2026-09-07 11:11:08 -0400

    Finish same Tree

---
+++ LeetCodeAnswers/100-Same-Tree/Solution.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCodeAnswers._100_Same_Tree;
+using Utilities;
+public class Solution
+{
+    public bool IsSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null)
+        {
+            return true;
+        }
+        else if ((p == null && q != null) || (p != null && q == null))
+        {
+            return false;
+        }
+        else
+        {
+            if (p.val == q.val)
+            {
+                bool firstVal  = IsSameTree(p.left, q.left);
+                bool secondVal = IsSameTree(p.right, q.right);
+                
+                return firstVal && secondVal;
+            }
+        }
+        return false;
+    }
+}
