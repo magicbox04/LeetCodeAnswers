58757dfe06691aa38a15198e6a44674fd395ced8
Sangjae Lee <robin040416@gmail.com>
2026-09-08 12:40:33 -0400

    try 572 Subtree of Another Tree

---
+++ LeetCodeAnswers/572-Subtree-of-Another-Tree/Solution.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCodeAnswers._572_Subtree_of_Another_Tree;
+using Utilities;
+// https://leetcode.com/problems/subtree-of-another-tree/description/
+public class Solution
+{
+    public bool IsSubtree(TreeNode root, TreeNode subRoot) {
+        if (root == null) 
+            return false;
+        if (IsSameTree(root, subRoot)) 
+            return true;
+        return IsSubtree(root.left, subRoot) || IsSubtree(root.right, subRoot);
+    }
+
+    private bool IsSameTree(TreeNode a, TreeNode b) {
+        if (a == null && b == null) 
+            return true;
+        if (a == null || b == null) 
+            return false;
+        if (a.val != b.val) 
+            return false;
+        return IsSameTree(a.left, b.left) && IsSameTree(a.right, b.right);
+    }
+}
