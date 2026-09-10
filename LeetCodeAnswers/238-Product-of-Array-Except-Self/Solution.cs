bda61b5ed4f989253c67cb77fe76f8af9c953f46
Sangjae Lee <slee2883@uwo.ca>
2026-09-09 21:11:51 -0400

    try product except self.Did see the answers so going to try again tomorrow

---
+++ LeetCodeAnswers/238-Product-of-Array-Except-Self/Solution.cs
@@ -0,0 +1,23 @@
+namespace Utilities._238_Product_of_Array_Except_Self;
+// https://leetcode.com/problems/product-of-array-except-self/
+
+public class Solution
+{
+    public int[] ProductExceptSelf(int[] nums) {
+        int n = nums.Length;
+        int[] answer = new int[n];
+
+        answer[0] = 1;
+        for (int i = 1; i < n; i++) {
+            answer[i] = answer[i - 1] * nums[i - 1];
+        }
+
+        int rightProduct = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            answer[i] *= rightProduct;
+            rightProduct *= nums[i];
+        }
+
+        return answer;
+    }
+}
