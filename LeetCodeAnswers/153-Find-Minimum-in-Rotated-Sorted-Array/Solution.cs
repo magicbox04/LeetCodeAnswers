adc2e5f28ce8d68250586c4b24da2af7e3efbeb8
Sangjae Lee <robin040416@gmail.com>
2026-09-13 12:03:04 -0400

    do find minimum in rotated sorted array

---
+++ LeetCodeAnswers/153-Find-Minimum-in-Rotated-Sorted-Array/Solution.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCodeAnswers._153_Find_Minimum_in_Rotated_Sorted_Array;
+// https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
+public class Solution
+{
+    public int FindMin(int[] nums)
+    {
+        int left = 0;
+        int right = nums.Length - 1;
+        int midPoint = 0;
+        while (left <= right)
+        {
+            midPoint = left + (right - left) / 2;
+
+            if (nums[midPoint] < nums[right])
+            {
+                right = midPoint;
+            }
+            else
+            {
+                left = midPoint + 1;
+                
+            }
+        }
+        return nums[midPoint];
+    }
+}
--- LeetCodeAnswers/238-Product-of-Array-Except-Self/Solution.cs
+++ LeetCodeAnswers/238-Product-of-Array-Except-Self/Solution.cs
@@ -3,21 +3,22 @@ namespace Utilities._238_Product_of_Array_Except_Self;
 
 public class Solution
 {
-    public int[] ProductExceptSelf(int[] nums) {
+    public int[] ProductExceptSelf(int[] nums)
+    {
+        
         int [] result = new int[nums.Length];
         result[0] = 1;
         for (int i = 1; i < nums.Length; i++)
         {
             result[i] = result[i - 1] * nums[i - 1];
         }
-        
+
         int rightProduct = 1;
         for (int i = nums.Length - 2; i >= 0; i--)
         {
-            result[i] *= rightProduct * nums[i + 1];
             rightProduct *= nums[i + 1];
+            result[i] *= rightProduct;
         }
-
         return result;
     }
 }
