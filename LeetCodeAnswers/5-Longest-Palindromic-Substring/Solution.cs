a6539f3bc2e7dfa874f5d5f123f3ea1bea278bfd
Sangjae Lee <robin040416@gmail.com>
2026-09-15 10:31:03 -0400

    Solve longest palindromic substring

---
+++ LeetCodeAnswers/5-Longest-Palindromic-Substring/Solution.cs
@@ -0,0 +1,38 @@
+﻿namespace LeetCodeAnswers._5_Longest_Palindromic_Substring;
+// https://leetcode.com/problems/longest-palindromic-substring/
+public class Solution
+{
+    public string LongestPalindrome(string s)
+    {
+        int longestLeft = 0;
+        int longestRight = 0;
+        for (int midPoint = 0; midPoint < s.Length; midPoint++)
+        {
+            (int localRight, int localLeft) = expand(s,midPoint, midPoint);
+            if (localRight - localLeft > longestRight - longestLeft)
+            {
+                longestLeft = localLeft;
+                longestRight = localRight;
+            }
+            
+            (localRight, localLeft) = expand(s,midPoint, midPoint + 1);
+            if (localRight - localLeft > longestRight - longestLeft)
+            {
+                longestLeft = localLeft;
+                longestRight = localRight;
+            }
+        }
+        return s.Substring(longestLeft, longestRight - longestLeft + 1);
+    }
+
+    private (int right, int left) expand(string s, int left, int right)
+    {
+        while (left >= 0 && right < s.Length && s[left] == s[right])
+        {
+            left--;
+            right++;
+        }
+
+        return (right - 1, left + 1);
+    }
+}
