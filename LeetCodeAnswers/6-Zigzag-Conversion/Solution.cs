86f78737c4bb068c06ce5e511eec4c7e1d60c94a
Sangjae Lee <robin040416@gmail.com>
2026-09-16 15:34:29 -0400

    Do zigzag conversion

---
+++ LeetCodeAnswers/6-Zigzag-Conversion/Solution.cs
@@ -0,0 +1,36 @@
+﻿namespace LeetCodeAnswers._6_Zigzag_Conversion;
+using System.Text;
+
+public class Solution
+{
+    public string Convert(string s, int numRows)
+    {
+        if (numRows == 1)
+            return s;
+        StringBuilder[] rows = new StringBuilder[numRows];       
+        for (int i = 0; i < numRows; i++)  {
+            rows[i] = new StringBuilder();
+        }
+
+        int curRow = 0;
+        bool goingDown = false;
+
+        foreach (char c in s)
+        {
+            rows[curRow].Append(c);
+            if (curRow == 0 || curRow == numRows - 1)
+            {
+                goingDown = !goingDown;
+            }
+            curRow += goingDown ? 1 : -1;
+        }
+        
+        StringBuilder result = new StringBuilder();
+
+        foreach (var row in rows)
+        {
+            result.Append(row);
+        }
+        return result.ToString();
+    }
+}
