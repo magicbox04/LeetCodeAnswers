3cb1b59507c24dc0e0d1709f038a7900dfbac043
Sangjae Lee <robin040416@gmail.com>
2026-09-25 13:43:51 -0400

    Do Is Valid BST

---
+++ LeetCodeAnswers/98-Validate-Binary-Search-Tree/Solution.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCodeAnswers._98_Validate_Binary_Search_Tree;
+using Utilities;
+
+// https://leetcode.com/problems/validate-binary-search-tree/
+
+public class Solution
+{
+    public bool IsValidBST(TreeNode root)
+    {
+        return Validate(root, long.MinValue, long.MaxValue);
+    }
+
+    private bool Validate(TreeNode node, long min, long max)
+    {
+        if (node == null)
+        {
+            return true;
+        }
+
+        if (node.val <= min || node.val >= max)
+        {
+            return false;
+        }
+
+        return Validate(node.left, min, node.val) &&
+               Validate(node.right, node.val, max);
+    }
+}
